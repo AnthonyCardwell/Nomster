9500b8154d34d4b3bf4ffedfe23121bc286dcde6
AnthonyCardwell <cardwellanthony@yahoo.com>
2019-06-17 21:31:22 +0000

    trying to add image2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,16 +3,19 @@ class PhotosController < ApplicationController
   def index
   end
 
-  def create
-        @place = Place.find(params[:place_id])
-    @place.photos.create(photos_params.merge(user: current_user))
+def create
+    @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
-
 def show
       @place = Place.find(params[:id])
       @comment = Comment.new
       @photo = Photo.new
+  end
+
+  def photo_params
+    params.require(:photo).permit(:Picture)
   end
 
 end
